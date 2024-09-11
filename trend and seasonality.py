b6375b9aa94d1585153e8f02e3b95763eab84601
divyadharshini-dd-02 <127177037@sastra.ac.in>
2024-09-11 21:32:38 +0530

    Add files via upload

---
+++ trend and seasonality.py
@@ -0,0 +1,18 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: C:/Users/divya/AppData/Local/Programs/Python/Python311/vipin 2.py ==
+Trend: [22.0, 18.5, 15.0, 17.666666666666668, 20.0, 22.333333333333332, 25.0, 27.666666666666668, 45.0, 62.0]
+Seasonality: [0.45454545454545453, 0.6486486486486487, 1.0, 1.0188679245283019, 1.0, 0.9850746268656717, 1.0, 1.0120481927710843, 0.6666666666666666, 0.5161290322580645]
+>>> 
+== RESTART: C:/Users/divya/AppData/Local/Programs/Python/Python311/vipin 2.py ==
+Trend: [22.0, 18.5, 15.0, 17.666666666666668, 20.0, 22.333333333333332, 25.0, 27.666666666666668, 45.0, 62.0]
+Seasonality: [0.45454545454545453, 0.6486486486486487, 1.0, 1.0188679245283019, 1.0, 0.9850746268656717, 1.0, 1.0120481927710843, 0.6666666666666666, 0.5161290322580645]
+Traceback (most recent call last):
+  File "C:/Users/divya/AppData/Local/Programs/Python/Python311/vipin 2.py", line 32, in <module>
+    plt("trend","seasonality")
+NameError: name 'plt' is not defined
+>>> 
+== RESTART: C:/Users/divya/AppData/Local/Programs/Python/Python311/vipin 2.py ==
+Trend: [22.0, 18.5, 15.0, 17.666666666666668, 20.0, 22.333333333333332, 25.0, 27.666666666666668, 45.0, 62.0]
+Seasonality: [0.45454545454545453, 0.6486486486486487, 1.0, 1.0188679245283019, 1.0, 0.9850746268656717, 1.0, 1.0120481927710843, 0.6666666666666666, 0.5161290322580645]
